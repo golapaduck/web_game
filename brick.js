f74f2ec14ea5b288fdba326f58e0efa5466d0954
golapaduck <golapaduck29@naver.com>
2022-07-21 14:54:08 +0000

    (공이 안 움직임)

--- brick.js
+++ brick.js
@@ -10,49 +10,25 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 
+var brickRowCount = 3;
+var brickColumnCount =5;
+var brickWidth =75;
+var brickHeight =20;
+var brickPadding =10;
+var brickOffsetTop =30;
+var brickOffsetLeft =30;
+
+
 var rightPressed = false;
 var leftPressed = false;
 
-
-function drawBall(){
-	ctx.beginPath();
-	ctx.arc(x,y,ballRadius,0,Math.PI*2);
-	ctx.fillStyle = "#0095DD";
-	ctx.fill();
-	ctx.closePath();
-}
-
-function drawPaddle(){
-	ctx.beginPath();
-	ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
-	ctx.fillStyle = "#0095DD";
-	ctx.fill();
-	ctx.closePath();
-}
-
-function draw(){
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	drawBall();	
-	drawPaddle();
-	
-	if(x+dx<ballRadius||x+dx>canvas.width-ballRadius){
-		dx=-dx;
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++){
+	bricks[c]=[];
+	for(var r=0; r<brickRowCount; r++){
+		bricks[c][r] = {x: 0,y: 0, status: 1 };
 	}
-	
-	if(y+dy<ballRadius||y+dy>canvas.width-ballRadius){
-		dy=-dy;
-	}
-	
-	if(rightPressed && paddleX < canvas.width-paddleWidth){
-		paddleX +=7;
-	}else if(leftPressed && paddleX > 0){
-		paddleX -=7;
-	}
-	
-	x += dx;
-	y += dy;
 }
-
 
 document.addEventListener("keydown",keyDownHandler,false);
 document.addEventListener("keyup",keyUpHandler,false);
@@ -75,5 +51,96 @@ function keyUpHandler(e){
 	}
 }
 
-setInterval(draw,10);
+
+function collisionDetection(){
+	for(var c=0; c<brickColumnCount; c++){
+		for(var r=0; brickRowCount; r++){
+			var b = bricks[c][r];
+			if(b.status == 1){
+				if(x>b.x && x < b.x+ brickWidth && y > b.y && y < b.y+brickHeight){
+					dy=-dy;
+					b.status =0;
+				}
+			}
+		}
+	}
+}
+
+
+
+function drawBall(){
+	ctx.beginPath();
+	ctx.arc(x,y,ballRadius,0,Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawBricks(){
+	for(var c=0; c<brickColumnCount; c++){
+		for(var r=0; r<brickRowCount; r++){
+			if(bricks[c][r].status ==1){
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
 			
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX,brickY,brickWidth,brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+			}
+		}
+	}
+}
+
+
+function draw(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	drawBricks();
+	drawBall();	
+	drawPaddle();
+	
+	collisionDetection();
+	
+	if(x+dx<ballRadius||x+dx>canvas.width-ballRadius){
+		dx=-dx;
+	}
+	
+	if(y+dy<ballRadius){
+		dy = -dy;
+	}else if (y+dy>canvas.height-ballRadius){
+		if(x>paddleX && x<paddleX+paddleWidth){
+			dy = -dy;
+		}
+		else{
+			alert("GAME OVER");
+			document.location.reload();
+		}
+	}
+	
+	if(rightPressed && paddleX < canvas.width-paddleWidth){
+		paddleX +=7;
+	}else if(leftPressed && paddleX > 0){
+		paddleX -=7;
+	}
+	
+	
+	
+	x += dx;
+	y += dy;
+}
+
+
+
+
+setInterval(draw,10);
